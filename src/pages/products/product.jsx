64f3d4203eb1302c587b903ec733b58db3852df2
AsadbekNurberdiyev <asadbeknurberdiyev31@gmail.com>
2025-05-04 20:50:52 +0500

    first commit

--- src/pages/products/product.jsx
+++ src/pages/products/product.jsx
@@ -10,7 +10,7 @@ const ProductCard = ({ product }) => {
       <img src={product.image} alt={product.title} className="h-40 object-contain mb-2" />
       <h2 className="font-semibold">{product.title}</h2>
       <p className="text-sm text-gray-600">{product.price.toLocaleString()} so'm</p>
-      <button onClick={() => addToCart(product)} className="mt-2 flex items-center gap-1 bg-orange-400 hover:bg-orange-500 text-white px-3 py-1 rounded">
+      <button onClick={() => addToCart(product)} className="mt-2 flex items-center gap-1 bg-indigo-400 hover:bg-indigo-500 text-white px-3 py-1 rounded">
         <ShoppingCart size={16} /> Qo'shish
       </button>
     </div>
